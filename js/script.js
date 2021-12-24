4711325449c05f0d4fcd2c2c5c17b3abbe64f5cd
kjassi560 <19757624+kjassi560@users.noreply.github.com>
2021-12-24 20:08:00 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-    $(".banking").hide();
-    $(".ssc-exams").show();
-    $("#ssc").click(function() {
-      $(".banking").hide();
-      $(".ssc-exams").show();
-    })
-    $("#banking").click(function() {
-      $(".ssc-exams").hide();
-      $(".banking").show();
-    })
+
